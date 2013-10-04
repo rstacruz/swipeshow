c8d7b15b0d158fdb68c829952af99bb894ebe227
Rico Sta. Cruz <hi@ricostacruz.com>
2013-10-04 19:47:05 +0800

    Bump version.

--- jquery.swipeshow.js
+++ jquery.swipeshow.js
@@ -57,7 +57,7 @@
 
   $.swipeshow = {};
 
-  $.swipeshow.version = "0.10.3";
+  $.swipeshow.version = "0.10.4";
 
   // Detect transition support, jQuery 1.8+ style.
   var transitions = typeof $("<div>").css({transition: 'all'}).css('transition') == 'string';
